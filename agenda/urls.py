d87de117258d4cdd90168b6ff10ef46e8c79a287
ueslei <uesleijf@gmail.com>
2019-11-07 09:49:01 -0300

    - configure agenda/urls

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from core.views import AgendaViewSet
+
+router = routers.DefaultRouter()
+router.register(r'agenda', AgendaViewSet, base_name='Agenda')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
