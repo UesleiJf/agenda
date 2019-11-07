041597297f143b5262f190209a1d540bf653b1b7
ueslei <uesleijf@gmail.com>
2019-11-07 09:48:17 -0300

    - configure core/admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Agenda
 
-# Register your models here.
+
+class AgendaAdmin(admin.ModelAdmin):
+    list_display = ['local', 'data', 'horario', 'valor', 'feito', 'pago']
+
+
+admin.site.register(Agenda, AgendaAdmin)
