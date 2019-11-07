17059cdb53d6244a10e7e1b3f70c309d1ba8729c
ueslei <uesleijf@gmail.com>
2019-11-07 09:45:29 -0300

    - Create models

--- core/models.py
+++ core/models.py
@@ -6,10 +6,17 @@ class Agenda(models.Model):
     local = models.TextField()
     data = models.DateField()
     horario = models.TimeField()
-    valor = models.DecimalField(max_digits=5, decimal_places=2)
-    responsavel = models.TextField()
-    contato = models.TextField()
+    valor = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    responsavel = models.TextField(null=True, blank=True)
+    contato = models.TextField(null=True, blank=True)
     feito = models.BooleanField(default=False)
+    pago = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Agenda Projeto Cordas'
+        verbose_name_plural = 'Agenda Projeto Cordas'
+        unique_together = ('data', 'horario')
+        ordering = ['data', 'horario']
 
     def __str__(self):
         return self.local
